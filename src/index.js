6df5d89c55d86e30fe901114e8293515229930f2
Joeypph <joseant_1098@hotmail.com>
2021-03-02 16:42:10 -0600

    Functional Components Practice

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+function MyInfo() {
+  return(
+    <div>
+      <h1>José Antonio Lome </h1>
+      <p>Paragraph</p>
+      <ul>
+        <li>Brazil</li>
+        <li>Canada</li>
+        <li>Europe</li>
+      </ul>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode><MyInfo /></React.StrictMode>,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
