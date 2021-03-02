1e2f5eda84f2faa7fb0eeebe4dbf73b1901240e7
Joeypph <joseant_1098@hotmail.com>
2021-03-02 16:05:02 -0600

    React Functional Components fix jsx, forgot a div

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
     </div>
   );
 }
