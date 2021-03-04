d7521058166758b02432f918fae90562f55b71e5
Joeypph <joseant_1098@hotmail.com>
2021-03-04 17:14:30 -0600

    JSX to Javascriptt and Back

--- src/App.js
+++ src/App.js
@@ -6,12 +6,29 @@ import MainContent from "./components/MainContent"
 import Footer from "./components/Footer"
 
 function App(){
+    /*const firstName = "Bob"
+    const lastName = "Ziroll"*/
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    
+    if (hours < 12) {
+      timeOfDay = "morning"
+    } else if (hours >= 12 && hours <= 19) {
+      timeOfDay = "afternoon"
+    } else {
+      timeOfDay = "night"
+    }
+
     return(
-        <div>
+        //<h1>Hello {firstName + " " + lastName}!</h1>
+        <h1>Good {timeOfDay}!</h1>
+
+        /*<div>
             <NavBar/>
             <MainContent/>
             <Footer/>
-        </div>
+        </div>*/
         
         /*<div>
             <input type="checkbox" name="chb1" />
