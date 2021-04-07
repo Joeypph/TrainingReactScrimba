08290439adae33616bd9b6c9aaa0e90977b70107
Joeypph <joseant_1098@hotmail.com>
2021-04-06 22:37:14 -0500

    35 React Todo App:phase 6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 
-import React, {Component} from "react"
+import React from "react"
+import TodoItem from "./components/TodoItem"
+import todosData from "./todosData"
 
-//import todosData from "./todosData"
-//import TodoItem from "./components/TodoItem"
 //import Joke from   "./components/Joke"
 //import jokesData from "./jokesData"
 //import Product from "./components/Product"
@@ -206,7 +206,7 @@ function App() {
             <button onClick={handleClick}>Click me</button>
     </div>
     )
-}*/
+}
 
 class App extends Component{
     constructor(){
@@ -255,5 +255,40 @@ class App extends Component{
             </div>
         )
     }
+}*/
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
+    
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+        
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )    
+    }
 }
+
 export default App
